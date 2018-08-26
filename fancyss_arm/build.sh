eca6025bebf65d74d3b5c9ddee9938f89b7dcea7
hq450 <qhuang450@gmail.com>
2018-08-26 19:39:34 +0800

    update 4.0.3 test

--- fancyss_arm/build.sh
+++ fancyss_arm/build.sh
@@ -6,6 +6,44 @@ TITLE=科学上网
 DESCRIPTION=科学上网
 HOME_URL=Main_Ss_Content.asp
 
+
+do_build_result() {
+if [ "$VERSION" = "" ]; then
+	echo "version not found"
+	exit 3
+fi
+
+rm -f ${MODULE}.tar.gz
+#清理mac os 下文件
+rm -f $MODULE/.DS_Store
+rm -f $MODULE/*/.DS_Store
+
+rm -f ${MODULE}.tar.gz
+tar -zcvf ${MODULE}.tar.gz $MODULE
+md5value=`md5sum ${MODULE}.tar.gz|tr " " "\n"|sed -n 1p`
+cat > ./version <<EOF
+$VERSION
+$md5value
+EOF
+cat version
+
+DATE=`date +%Y-%m-%d_%H:%M:%S`
+cat > ./config.json.js <<EOF
+{
+"version":"$VERSION",
+"md5":"$md5value",
+"home_url":"$HOME_URL",
+"title":"$TITLE",
+"description":"$DESCRIPTION",
+"build_date":"$DATE"
+}
+EOF
+
+#update md5
+# python ../softcenter/gen_install.py stage2
+}
+
+
 # Check and include base
 DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 if [ "$MODULE" == "" ]; then
@@ -19,7 +57,7 @@ if [ -f "$DIR/$MODULE/$MODULE/install.sh" ]; then
 fi
 
 # now include build_base.sh
-. $DIR/../softcenter/build_base.sh
+# . $DIR/../softcenter/build_base.sh
 
 # change to module directory
 cd $DIR
--- fancyss_arm/config.json.js
+++ fancyss_arm/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"4.0.1",
-"md5":"1a2e090fa71e56def7f9340298026f23",
+"version":"4.0.3",
+"md5":"5716baf2e8719d5134c2fa5a1e854998",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2018-08-21_23:40:37"
+"build_date":"2018-08-26_19:37:40"
 }
